54579a734d35b97409769f252ecd8f3d0783f8b0
xavierrobin <xrobin7@gmail.com>
2022-11-20 18:09:29 +0000

    new change on homepage

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Test
+          Test website
         </h1>
 
         <p className={styles.description}>
